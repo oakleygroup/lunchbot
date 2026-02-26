0d980ebe04a55dd8ee35439a4822e5da9105ed87
meaganoak <104146024+meaganoak@users.noreply.github.com>
2026-02-26 15:40:54 +0000

    lunchbot python

---
+++ lunchbot.py
@@ -0,0 +1,15 @@
+import zulip
+from datetime import datetime
+
+client = zulip.Client(
+    email="lunch-bot@oakleygroup.zulipchat.com",
+    api_key="Mfy70yNDzTemShSL5vz9PtUVzPKEIM1k",
+    site="https://oakleygroup.zulipchat.com",
+)"
+
+response = client.send_message({
+    "type": "stream",
+    "to": "general",        # Channel name
+    "topic": "lunch",       # Topic name
+    "content": "lunch in breakroom",
+})
